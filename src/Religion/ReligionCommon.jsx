fdb64a67da51f5fc88c9ceb374f613043eb3a5a7
Mostakim-billah <mostakimbillahn210@gmail.com>
2021-10-20 20:19:19 -0800

    this is my proper protfulio website

---
+++ src/Religion/ReligionCommon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const ReligionCommon = (props) => {
+    return(
+        <>
+        <div className="col-md-4 col-sm-6 col-lg-4 col-11">
+            <img src={props.imgReli} className="img-fluid" alt="imgpro..." /> 
+        </div> 
+        </>
+    )
+}
+export default ReligionCommon;
